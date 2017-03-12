8dcbd6649f71f036eecd4869a4d85ba23040c8a1
yuandiandengdai <2385812567@qq.com>
2017-03-13 00:19:35 +0800

    后台数据管理

--- App/Modules/Admin/Action/RoomAction.class.php
+++ App/Modules/Admin/Action/RoomAction.class.php
@@ -8,6 +8,14 @@
  */
 class RoomAction extends Action{
     public function index(){
-        $this->display();
+        import('ORG.Util.Page');// 导入分页类
+        $count = D('Room')->count();// 查询满足要求的总记录数
+        $Page = new Page($count, 8);// 实例化分页类 传入总记录数和每页显示的记录数
+        $show = $Page->show();// 分页显示输出
+        // 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+        $data = D('Room')->order('id')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('data', $data);// 赋值数据集
+        $this->assign('page', $show);// 赋值分页输出
+        $this->display(); // 输出模板
     }
 }
