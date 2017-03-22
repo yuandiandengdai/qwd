46c361c78ed30afbb0495fad07a3c544940238a3
yuandiandengdai <23858125672qq.com>
2017-03-22 18:54:24 +0800

    页脚修改

---
+++ App/Modules/Home/Action/HelpAction.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: yuandian
+ * Date: 2017-03-22
+ * Time: 18:49
+ */
+class HelpAction extends Action{
+    public function index(){
+        $this->display();
+    }
+}
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yuandian
+ * Date: 2017-03-15
+ * Time: 14:44
+ */
+echo md5(8432);
+//117ffc1acd844e431a4b73f0867adae5
