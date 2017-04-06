6c1b7d037c98869cdc72b5c56f71260c04a67fe8
yuandiandengdai <2385812567@qq.com>
2017-04-07 01:17:00 +0800

    修改图形验证码样式

--- ThinkPHP/Extend/Library/ORG/Util/Image.class.php
+++ ThinkPHP/Extend/Library/ORG/Util/Image.class.php
@@ -300,7 +300,6 @@ class Image {
         }
         return false;
     }
-    
     /**
      * 根据给定的字符串生成图像
      * @static
@@ -378,25 +377,28 @@ class Image {
         } else {
             $im = imagecreate($width, $height);
         }
-        $r = Array(225, 255, 255, 223);
-        $g = Array(225, 236, 237, 255);
-        $b = Array(225, 236, 166, 125);
+        /*$r = Array(225, 205, 205, 223);
+        $g = Array(225, 206, 207, 255);
+        $b = Array(225, 206, 106, 125);*/
+
         $key = mt_rand(0, 3);
 
-        $backColor = imagecolorallocate($im, $r[$key], $g[$key], $b[$key]);    //背景色（随机）
-        $borderColor = imagecolorallocate($im, 100, 100, 100);                    //边框色
+        $backColor = imagecolorallocate($im, 246, 246, 246);    //背景色（随机）
+        $borderColor = imagecolorallocate($im, 233, 233, 233);                    //边框色
         imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $backColor);
         imagerectangle($im, 0, 0, $width - 1, $height - 1, $borderColor);
         $stringColor = imagecolorallocate($im, mt_rand(0, 200), mt_rand(0, 120), mt_rand(0, 120));
         // 干扰
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 3; $i++) {
             imagearc($im, mt_rand(-10, $width), mt_rand(-10, $height), mt_rand(30, 300), mt_rand(20, 200), 55, 44, $stringColor);
         }
-        for ($i = 0; $i < 25; $i++) {
-            imagesetpixel($im, mt_rand(0, $width), mt_rand(0, $height), $stringColor);
-        }
+        //for ($i = 0; $i < 3; $i++) {
+        //   imagesetpixel($im, mt_rand(0, $width), mt_rand(0, $height), $stringColor);
+        //}
+
+
         for ($i = 0; $i < $length; $i++) {
-            imagestring($im, 5, $i * 10 + 5, mt_rand(1, 8), $randval{$i}, $stringColor);
+            imagestring($im, 5, $i * 25 + 5, mt_rand(2, 8), $randval{$i}, $stringColor);
         }
         Image::output($im, $type);
     }
